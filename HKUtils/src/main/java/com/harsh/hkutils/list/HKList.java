ca07998a657df1cdf94445450de9e5c7cc3888db
harsh <harshgkanjariya2468@gmail.com>
2021-01-25 12:52:54 +0530

    hklist recyclerview public

--- HKUtils/src/main/java/com/harsh/hkutils/list/HKList.java
+++ HKUtils/src/main/java/com/harsh/hkutils/list/HKList.java
@@ -29,7 +29,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class HKList extends RelativeLayout {
-	private RecyclerView recyclerView;
+	public RecyclerView recyclerView;
 	private HKAdapter<?> adapter;
 
 	private LinearLayout emptyLayout;
