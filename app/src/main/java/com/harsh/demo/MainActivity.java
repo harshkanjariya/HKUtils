e10c95fd433701e5e571fb25a8af72ec728af058
Harsh Kanjariya <harshgkanjariya2468@gmail.com>
2021-10-31 10:03:32 +0530

    JSON param set

--- app/src/main/java/com/harsh/demo/MainActivity.java
+++ app/src/main/java/com/harsh/demo/MainActivity.java
@@ -7,9 +7,16 @@ import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.MenuItem;
 
 import com.google.android.material.navigation.NavigationView;
+import com.harsh.hkutils.HttpProcess;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -50,5 +57,17 @@ public class MainActivity extends AppCompatActivity {
 				return true;
 			}
 		});
+		test();
+	}
+	void test(){
+		HttpProcess process = new HttpProcess("http://192.168.0.101:4000/health");
+		process.post(new HttpProcess.Callback() {
+			@Override
+			public void onError(IOException exception) { }
+			@Override
+			public void onResponse(JSONObject json) throws JSONException {
+				Log.e("jasjd", "onResponse: "+json.toString());
+			}
+		},"{\"ab\":\"cd\"}");
 	}
 }
